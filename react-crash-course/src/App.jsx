7b5f8539084151fffd272b028cffac55f49a4d57
YGourarie <yehudahgourarie@gmail.com>
2022-11-15 15:11:53 -0500

    crash course complete

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -1,13 +1,21 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import About from "./Pages/About.jsx";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Nav from "./components/Nav"
+import Users from "./Pages/Users";
 
 function App() {
   return (
     <div>
       <Router>
+        <Nav />
         <Routes>
-          <Route path="/" element={<About />}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/users/:id" element={<Users />}></Route>
         </Routes>
       </Router>
     </div>
